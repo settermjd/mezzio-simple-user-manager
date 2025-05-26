fcf7e14469e7388aee62420d29b7be7982111d45
Matthew Setter <matthew@matthewsetter.com>
2025-05-26 11:27:05 +1000

    Minor improvement to RedirectFromForgotPasswordPolicy
    Just adds some documentation and fixes up the case of a member property.

--- src/Policy/RedirectFromForgotPasswordPolicy.php
+++ src/Policy/RedirectFromForgotPasswordPolicy.php
@@ -6,12 +6,19 @@ namespace SimpleUserManager\Policy;
 
 use Laminas\Authentication\AuthenticationServiceInterface;
 
+/**
+ * This class handles redirections from the application's ForgotPassword route.
+ *
+ * A redirect is required if there is already a logged-in user when a request
+ * to the forgot password route is made.
+ */
 readonly class RedirectFromForgotPasswordPolicy implements RoutePolicyInterface
 {
     public function __construct(
         private AuthenticationServiceInterface $authService,
-        private string $redirectToURI
-    ) {}
+        private string $redirectToUri
+    ) {
+    }
 
     /**
      * @inheritDoc
@@ -29,6 +36,6 @@ readonly class RedirectFromForgotPasswordPolicy implements RoutePolicyInterface
      */
     public function getRedirectURI(): string
     {
-        return $this->redirectToURI;
+        return $this->redirectToUri;
     }
 }
